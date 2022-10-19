3a8f4cae21a7e1fd46d9d4bcfe8d7ce66df8425c
vikto <vikto@LAPTOP-5FL58TMM>
2022-10-19 15:42:58 +0200

    Eine Entschlackung der Importierungen

--- src/main/java/de_die/gfi/viktor/baresic/javafx/jdbc/PlzEinwohnerController.java
+++ src/main/java/de_die/gfi/viktor/baresic/javafx/jdbc/PlzEinwohnerController.java
@@ -17,7 +17,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
