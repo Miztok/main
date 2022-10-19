48c5b9237be7528d3364a6e7b9f3db7887a2921a
vikto <vikto@LAPTOP-5FL58TMM>
2022-10-19 10:57:51 +0200

    Formattierung

--- src/main/java/de_die/gfi/viktor/baresic/javafx/jdbc/PlzEinwohnerController.java
+++ src/main/java/de_die/gfi/viktor/baresic/javafx/jdbc/PlzEinwohnerController.java
@@ -21,188 +21,187 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class PlzEinwohnerController{
-	int bevoelkerungAnOrtOderPlz=0;
-	double flaecheVonOrtPlzsOderPlz=0.0;
-	static List<PlzEinwohnerEintrag>listeDerEinwohnerproPlz=new ArrayList<PlzEinwohnerEintrag>();
+public class PlzEinwohnerController {
+	int bevoelkerungAnOrtOderPlz = 0;
+	double flaecheVonOrtPlzsOderPlz = 0.0;
+	static List<PlzEinwohnerEintrag> listeDerEinwohnerproPlz = new ArrayList<PlzEinwohnerEintrag>();
 	@FXML
-    private TextField tfPLZOrt;
+	private TextField tfPLZOrt;
 	@FXML
-    private Button buttonClose;
+	private Button buttonClose;
 
-    @FXML
-    private Button buttonOK;
-    
-    @FXML
-    private Label labelBevoelkerung;
+	@FXML
+	private Button buttonOK;
 
-    @FXML
-    private Label labelBevoelkerungsdichte;
+	@FXML
+	private Label labelBevoelkerung;
 
-    @FXML
-    private Label labelFlaeche;
-    
-    @FXML
-    private Label labelPlzOrt;
+	@FXML
+	private Label labelBevoelkerungsdichte;
 
-    @FXML
-    private CheckBox checkBoxPLZAnzeigen;
+	@FXML
+	private Label labelFlaeche;
 
-    @FXML
-    void handleButtonClose(ActionEvent event) {
-    	stage.close();
-    }
-    
+	@FXML
+	private Label labelPlzOrt;
+
+	@FXML
+	private CheckBox checkBoxPLZAnzeigen;
+
+	@FXML
+	void handleButtonClose(ActionEvent event) {
+		stage.close();
+	}
+
 	private List<PlzEinwohnerEintrag> erzeugePlzListe(Connection c) throws SQLException {
-		Statement stmt=c.createStatement();
-    	ResultSet resultSet = stmt.executeQuery("SELECT * FROM plz_einwohner");  	
-    	
-    	List<PlzEinwohnerEintrag> liste = new ArrayList();
-    	while(resultSet.next()) {
-    		String plz=resultSet.getString("plz");
-    		String ort=resultSet.getString("ort");
-    		String einwohner=resultSet.getString("einwohner");
-    		String quadratkilometer=resultSet.getString("quadratkilometer");
-    		liste.add(new PlzEinwohnerEintrag(plz,ort,einwohner,quadratkilometer));
-    	}
-   
-    	System.out.println("Index 0 - 10: "+  liste.subList(0, 10));
-    	return liste;
+		Statement stmt = c.createStatement();
+		ResultSet resultSet = stmt.executeQuery("SELECT * FROM plz_einwohner");
+
+		List<PlzEinwohnerEintrag> liste = new ArrayList();
+		while (resultSet.next()) {
+			String plz = resultSet.getString("plz");
+			String ort = resultSet.getString("ort");
+			String einwohner = resultSet.getString("einwohner");
+			String quadratkilometer = resultSet.getString("quadratkilometer");
+			liste.add(new PlzEinwohnerEintrag(plz, ort, einwohner, quadratkilometer));
+		}
+
+		System.out.println("Index 0 - 10: " + liste.subList(0, 10));
+		return liste;
 	}
 
-    @FXML
-    void handleButtonOK(ActionEvent event) throws IOException, SQLException {
+	@FXML
+	void handleButtonOK(ActionEvent event) throws IOException, SQLException {
 		plzOderOrtDatenAusdruecken();
-    }
-    
-	private void plzOderOrtDatenAusdruecken() {
-		if(checkBoxPLZAnzeigen.isSelected()) {
-    		eintragFuerPLZBekommenAusdruckenUndBevoelkerungUndFlaecheBerechnen();
-    	}else {
-    		eintraegeFuerOrtBekommenAusdruckenUndBevoelkerungUndFlaecheBerechnen();
-    	}
-		labeslsSetzen(bevoelkerungAnOrtOderPlz, flaecheVonOrtPlzsOderPlz);
-		bevoelkerungAnOrtOderPlz=0;
-		flaecheVonOrtPlzsOderPlz=0.0;
 	}
+
+	private void plzOderOrtDatenAusdruecken() {
+		if (checkBoxPLZAnzeigen.isSelected()) {
+			eintragFuerPLZBekommenAusdruckenUndBevoelkerungUndFlaecheBerechnen();
+		} else {
+			eintraegeFuerOrtBekommenAusdruckenUndBevoelkerungUndFlaecheBerechnen();
+		}
+		labeslsSetzen(bevoelkerungAnOrtOderPlz, flaecheVonOrtPlzsOderPlz);
+		bevoelkerungAnOrtOderPlz = 0;
+		flaecheVonOrtPlzsOderPlz = 0.0;
+	}
+
 	private void eintragFuerPLZBekommenAusdruckenUndBevoelkerungUndFlaecheBerechnen() {
-		for(int i=0;i<listeDerEinwohnerproPlz.size();i++) {
-			if(listeDerEinwohnerproPlz.get(i).plz.equals(tfPLZOrt.getText())) {
+		for (int i = 0; i < listeDerEinwohnerproPlz.size(); i++) {
+			if (listeDerEinwohnerproPlz.get(i).plz.equals(tfPLZOrt.getText())) {
 				zugehoerigeEintraegeAusdruckenUndBevoelkuerungUndFlaecheAddieren(i);
 				labelPlzOrt.setText(listeDerEinwohnerproPlz.get(i).ort);
-			}else {
+			} else {
 				continue;
 			}
 		}
 	}
+
 	private void eintraegeFuerOrtBekommenAusdruckenUndBevoelkerungUndFlaecheBerechnen() {
-		for(int i=0;i<listeDerEinwohnerproPlz.size();i++) {
-			if(listeDerEinwohnerproPlz.get(i).ort.equals(tfPLZOrt.getText())) {
+		for (int i = 0; i < listeDerEinwohnerproPlz.size(); i++) {
+			if (listeDerEinwohnerproPlz.get(i).ort.equals(tfPLZOrt.getText())) {
 				zugehoerigeEintraegeAusdruckenUndBevoelkuerungUndFlaecheAddieren(i);
-			}else {
+			} else {
 				continue;
 			}
 		}
 		labelPlzOrt.setText(tfPLZOrt.getText());
 	}
-	
+
 	private void zugehoerigeEintraegeAusdruckenUndBevoelkuerungUndFlaecheAddieren(int i) {
 		bevoelkerungAnOrtOderPlz = zugehoerigeEintraegeAusdruckenUndBevoelkerungAddieren(bevoelkerungAnOrtOderPlz, i);
-		flaecheVonOrtPlzsOderPlz+=parseStringIntoDouble(listeDerEinwohnerproPlz.get(i).quadratkilometer);
+		flaecheVonOrtPlzsOderPlz += parseStringIntoDouble(listeDerEinwohnerproPlz.get(i).quadratkilometer);
 	}
-	
+
 	private void labeslsSetzen(int bevoelkerungAnOrtOderPlz, double flaecheVonOrtPlzsOderPlz) {
-		labelBevoelkerung.setText(""+bevoelkerungAnOrtOderPlz);
-		labelFlaeche.setText(""+flaecheVonOrtPlzsOderPlz);
-		labelBevoelkerungsdichte.setText(""+((bevoelkerungAnOrtOderPlz*1.0)/flaecheVonOrtPlzsOderPlz));
-		
+		labelBevoelkerung.setText("" + bevoelkerungAnOrtOderPlz);
+		labelFlaeche.setText("" + flaecheVonOrtPlzsOderPlz);
+		labelBevoelkerungsdichte.setText("" + ((bevoelkerungAnOrtOderPlz * 1.0) / flaecheVonOrtPlzsOderPlz));
+
 	}
-	
+
 	private int zugehoerigeEintraegeAusdruckenUndBevoelkerungAddieren(int bevoelkerungAnOrtOderPlz, int i) {
 		System.out.println(listeDerEinwohnerproPlz.get(i).toString());
-		bevoelkerungAnOrtOderPlz+=parseStringIntoInteger(listeDerEinwohnerproPlz.get(i).einwohner);
+		bevoelkerungAnOrtOderPlz += parseStringIntoInteger(listeDerEinwohnerproPlz.get(i).einwohner);
 		return bevoelkerungAnOrtOderPlz;
 	}
 
 	private double parseStringIntoDouble(String quadratkilometer) {
-		int kommaPunkt=0;
+		int kommaPunkt = 0;
 		kommaPunkt = kommaStelleBerechnen(quadratkilometer, kommaPunkt);
-		String[] quadratkilometerDoubleString=new String [2];
-		quadratkilometerDoubleString[0]=quadratkilometer.substring(0, kommaPunkt);
-		quadratkilometerDoubleString[1]=quadratkilometer.substring(kommaPunkt+1);
-		double flaeche=0.0;
-		for(int j=0;j<quadratkilometerDoubleString[0].length();j++) {
-			flaeche+=Math.pow(10, quadratkilometerDoubleString[0].length()-j-1)*(quadratkilometerDoubleString[0].charAt(j)-47);
+		String[] quadratkilometerDoubleString = new String[2];
+		quadratkilometerDoubleString[0] = quadratkilometer.substring(0, kommaPunkt);
+		quadratkilometerDoubleString[1] = quadratkilometer.substring(kommaPunkt + 1);
+		double flaeche = 0.0;
+		for (int j = 0; j < quadratkilometerDoubleString[0].length(); j++) {
+			flaeche += Math.pow(10, quadratkilometerDoubleString[0].length() - j - 1)
+					* (quadratkilometerDoubleString[0].charAt(j) - 47);
 		}
-		for(int j=0;j<quadratkilometerDoubleString[1].length();j++) {
-			flaeche+=Math.pow(10, -j-1)*(quadratkilometerDoubleString[1].charAt(j)-47);
+		for (int j = 0; j < quadratkilometerDoubleString[1].length(); j++) {
+			flaeche += Math.pow(10, -j - 1) * (quadratkilometerDoubleString[1].charAt(j) - 47);
 		}
 		return flaeche;
 	}
-	
+
 	private int kommaStelleBerechnen(String quadratkilometer, int kommaPunkt) {
-		for (int i=0;i<quadratkilometer.length();i++) {
-			if(quadratkilometer.charAt(i)=='.') {
-				kommaPunkt=i;
+		for (int i = 0; i < quadratkilometer.length(); i++) {
+			if (quadratkilometer.charAt(i) == '.') {
+				kommaPunkt = i;
 				break;
 			}
 		}
 		return kommaPunkt;
 	}
-	
+
 	private int parseStringIntoInteger(String einwohner) {
-		int laenge=einwohner.length();
-		int zahl=0;
-		for(int i=0;i<laenge;i++) {
-			zahl+=Math.pow(10, laenge-i-1)*(einwohner.charAt(i)-47);
+		int laenge = einwohner.length();
+		int zahl = 0;
+		for (int i = 0; i < laenge; i++) {
+			zahl += Math.pow(10, laenge - i - 1) * (einwohner.charAt(i) - 47);
 		}
-		return zahl;   
+		return zahl;
 	}
-	
+
 	private static Connection verbindungAufmachen() throws SQLException {
 		Connection c = DriverManager.getConnection(
 
-					"jdbc:mariadb://localhost:3019/newdatabase2",
+				"jdbc:mariadb://localhost:3019/newdatabase2",
 
-					"root", "Rkotrab8.7,0");
+				"root", "Rkotrab8.7,0");
 		return c;
 	}
 
-    
-	
-public static void showDialog() throws IOException, SQLException {
-		
-   	 	Stage stage = new Stage();
+	public static void showDialog() throws IOException, SQLException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("plz_einwohner.fxml"));
-        Parent parent = fxmlLoader.load();
-        
-        PlzEinwohnerController ctrl=fxmlLoader.getController();
-        Connection c=verbindungAufmachen();
-    	listeDerEinwohnerproPlz=ctrl.erzeugePlzListe(c);
-        ctrl=fxmlLoader.getController();
-        
-        ctrl.setStage(stage);
-        Scene scene = new Scene(parent);
-        stage.setScene(scene);
-        stage.showAndWait();
-        
-    }
+		Stage stage = new Stage();
 
-Stage stage;
+		FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("plz_einwohner.fxml"));
+		Parent parent = fxmlLoader.load();
 
-public Stage getStage() {
-	return stage;
-}
+		PlzEinwohnerController ctrl = fxmlLoader.getController();
+		Connection c = verbindungAufmachen();
+		listeDerEinwohnerproPlz = ctrl.erzeugePlzListe(c);
+		ctrl = fxmlLoader.getController();
 
-public void setStage(Stage stage) {
-	initialize();
-	this.stage = stage;
-}
+		ctrl.setStage(stage);
+		Scene scene = new Scene(parent);
+		stage.setScene(scene);
+		stage.showAndWait();
 
+	}
 
+	Stage stage;
 
-public void initialize() {
-	stage= new Stage();	
-}
+	public Stage getStage() {
+		return stage;
+	}
+
+	public void setStage(Stage stage) {
+		initialize();
+		this.stage = stage;
+	}
+
+	public void initialize() {
+		stage = new Stage();
+	}
 }
