3bd4689246155ab7e00781a48599fae00d331a14
vikto <vikto@LAPTOP-5FL58TMM>
2022-10-19 11:55:48 +0200

    Eine Berichtigung

--- src/main/java/de_die/gfi/viktor/baresic/javafx/jdbc/PlzEinwohnerController.java
+++ src/main/java/de_die/gfi/viktor/baresic/javafx/jdbc/PlzEinwohnerController.java
@@ -23,6 +23,9 @@ import javafx.stage.Stage;
 
 public class PlzEinwohnerController{
 	static List<PlzEinwohnerEintrag>listeDerEinwohnerproPlz=new ArrayList<PlzEinwohnerEintrag>();
+	int bevoelkerungAnOrtOderPlz=0;
+	double flaecheVonOrtPlzsOderPlz=0.0;
+	String ort="";
 	@FXML
     private TextField tfPLZOrt;
 	@FXML
@@ -39,6 +42,9 @@ public class PlzEinwohnerController{
 
     @FXML
     private Label labelFlaeche;
+    
+    @FXML
+    private Label labelPlzOrt;
 
     @FXML
     private CheckBox checkBoxPLZSuchen;
@@ -66,63 +72,86 @@ public class PlzEinwohnerController{
 	}
 
     @FXML
-    void handleButtonSearchPlacesOrPostalNumbers(ActionEvent event) throws IOException, SQLException {
-    	Connection c = verbindungAufmachen();
-		plzOderOrtDatenAusdruecken(c);
+    void handleButtonSearchPlacesOrPostalNumbers(ActionEvent event) throws IOException{
+		plzOderOrtDatenAusdruecken();
     }
-	private void plzOderOrtDatenAusdruecken(Connection c) throws SQLException {
-		int bevoelkerungAnOrtOderPlz=0;
-		double flaecheVonOrtPlzsOderPlz=0.0;
+	private void plzOderOrtDatenAusdruecken() {
+		bevoelkerungAnOrtOderPlz=0;
+		flaecheVonOrtPlzsOderPlz=0.0;
+		ort="";
 		if(checkBoxPLZSuchen.isSelected()) {
-    		for(int i=0;i<listeDerEinwohnerproPlz.size();i++) {
-    			if(listeDerEinwohnerproPlz.get(i).plz.equals(tfPLZOrt.getText())) {
-    				bevoelkerungAnOrtOderPlz = zugehoerigeEintraegeAusdruckenUndBevoelkerungAddieren(bevoelkerungAnOrtOderPlz, i);
-    				flaecheVonOrtPlzsOderPlz+=parseStringIntoDouble(listeDerEinwohnerproPlz.get(i).quadratkilometer);
-    			}else {
-    				continue;
-    			}
-    		}
+    		plzDatenBekommenUndAusdrucken();
     	}else {
-    		for(int i=0;i<listeDerEinwohnerproPlz.size();i++) {
-    			if(listeDerEinwohnerproPlz.get(i).ort.equals(tfPLZOrt.getText())) {
-    				bevoelkerungAnOrtOderPlz = zugehoerigeEintraegeAusdruckenUndBevoelkerungAddieren(bevoelkerungAnOrtOderPlz, i);
-    				flaecheVonOrtPlzsOderPlz+=parseStringIntoDouble(listeDerEinwohnerproPlz.get(i).quadratkilometer);
-    			}else {
-    				continue;
-    			}
-    		}
+    		ortDatenBekommenUndAusdrucken();
     	}
-		labeslsSetzen(bevoelkerungAnOrtOderPlz, flaecheVonOrtPlzsOderPlz);
+		labeslsSetzen(bevoelkerungAnOrtOderPlz, flaecheVonOrtPlzsOderPlz,ort);
 	}
-	private void labeslsSetzen(int bevoelkerungAnOrtOderPlz, double flaecheVonOrtPlzsOderPlz) {
+
+	private void ortDatenBekommenUndAusdrucken() {
+		for(int i=0;i<listeDerEinwohnerproPlz.size();i++) {
+			if(listeDerEinwohnerproPlz.get(i).ort.equals(tfPLZOrt.getText())) {
+				int bevoelkerungplz=zugehoerigeEintraegeAusdruckenUndBevoelkerungAddieren(listeDerEinwohnerproPlz.get(i).einwohner, i);
+				bevoelkerungAnOrtOderPlz += bevoelkerungplz;
+				flaecheVonOrtPlzsOderPlz+=parseStringIntoDouble(listeDerEinwohnerproPlz.get(i).quadratkilometer);
+			}else {
+				continue;
+			}
+		}
+		ort=tfPLZOrt.getText();
+	}
+
+	private void plzDatenBekommenUndAusdrucken() {
+		for(int i=0;i<listeDerEinwohnerproPlz.size();i++) {
+			if(listeDerEinwohnerproPlz.get(i).plz.equals(tfPLZOrt.getText())) {
+				int bevoelkerungplz=zugehoerigeEintraegeAusdruckenUndBevoelkerungAddieren(listeDerEinwohnerproPlz.get(i).einwohner, i);
+				bevoelkerungAnOrtOderPlz += bevoelkerungplz;
+				flaecheVonOrtPlzsOderPlz+=parseStringIntoDouble(listeDerEinwohnerproPlz.get(i).quadratkilometer);
+				ort=listeDerEinwohnerproPlz.get(i).ort;
+			}else {
+				continue;
+			}
+		}
+	}
+	private void labeslsSetzen(int bevoelkerungAnOrtOderPlz, double flaecheVonOrtPlzsOderPlz, String ort2) {
 		labelBevoelkerung.setText(""+bevoelkerungAnOrtOderPlz);
 		labelFlaeche.setText(""+flaecheVonOrtPlzsOderPlz);
 		labelBevoelkerungsdichte.setText(""+((bevoelkerungAnOrtOderPlz*1.0)/flaecheVonOrtPlzsOderPlz));
+		labelPlzOrt.setText(ort2);
 	}
-	private int zugehoerigeEintraegeAusdruckenUndBevoelkerungAddieren(int bevoelkerungAnOrtOderPlz, int i) {
+	private int zugehoerigeEintraegeAusdruckenUndBevoelkerungAddieren(String bevoelkerung, int i) {
 		System.out.println(listeDerEinwohnerproPlz.get(i).toString());
-		bevoelkerungAnOrtOderPlz+=parseStringIntoInteger(listeDerEinwohnerproPlz.get(i).einwohner);
+		int bevoelkerungAnOrtOderPlz=0;
+		bevoelkerungAnOrtOderPlz+=parseStringIntoInteger(bevoelkerung);
 		return bevoelkerungAnOrtOderPlz;
 	}
 
 	private double parseStringIntoDouble(String quadratkilometer) {
 		int kommaPunkt=0;
 		for (int i=0;i<quadratkilometer.length();i++) {
-			if(quadratkilometer.charAt(i)==',') {
+			if(quadratkilometer.charAt(i)=='.') {
 				kommaPunkt=i;
 			}
 		}
 		String[] quadratkilometerDoubleString=new String [2];
 		
-		return 1;
+		quadratkilometerDoubleString[0]=quadratkilometer.substring(0, kommaPunkt-1);
+		quadratkilometerDoubleString[1]=quadratkilometer.substring(kommaPunkt+1);
+		double flaeche=0.0;
+		for(int j=0;j<quadratkilometerDoubleString[0].length();j++) {
+			flaeche+=Math.pow(10.0, quadratkilometerDoubleString[0].length()-j-1)*(quadratkilometerDoubleString[0].charAt(j)-47);
+		}
+		for(int k=0;k<quadratkilometerDoubleString[1].length();k++) {
+			flaeche+=Math.pow(10.0, -k)*(quadratkilometerDoubleString[1].charAt(k)-47);
+		}
+		return flaeche;
 	}
 	private int parseStringIntoInteger(String einwohner) {
 		int laenge=einwohner.length();
 		int zahl=0;
 		for(int i=0;i<laenge;i++) {
-			zahl+=Math.pow(10, laenge-i)*(einwohner.charAt(i)-47);
+			zahl+=Math.pow(10, laenge-i-1)*(einwohner.charAt(i)-47);
 		}
-		return 0;   
+		return zahl;   
 	}
 	private static Connection verbindungAufmachen() throws SQLException {
 		Connection c = DriverManager.getConnection(
